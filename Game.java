a65cbe2fd70b345c66239ad006b905b3fd679746
Roman <willsteady@hotmail.com>
2019-10-16 09:57:47 +0200

    Revert "act silly"
    This reverts commit 4351a67e3775493650700547cf8072a49d84d561.

---
+++ Game.java
@@ -0,0 +1,15 @@
+import java.io.Console;
+
+public class Game {
+     public static void main (String[] args) {
+         System.out.println("Welcome to the Countries game.");
+Console console = System.console();
+String answer = console.readLine ("Which continent does ' Sweden ' belong to?");
+String continent = "Europe";
+if (answer.equals(continent)) {
+System.out.println("Correct!");
+} else { 
+System.out.println("No, the correct answer is " + continent);
+}
+}
+}

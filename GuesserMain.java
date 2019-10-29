90e0f776b92ea09ee86668cb991ff67cdd5b4a5a
willsteady <56724468+willsteady@users.noreply.github.com>
2019-10-29 21:55:27 +0100

    Update GuesserMain.java
    Updated the file GuesserMain.java so that user can choose which interval of numbers to choose. Added variables int ilow and int ihigh, which get send to the constructor when an object of the class Guesser get created.

--- GuesserMain.java
+++ GuesserMain.java
@@ -3,8 +3,10 @@ package gitProject;
 public class GuesserMain {
 
 	public static void main(String[] args) {
-		Guesser guesser = new Guesser(0, 1000);
-		guesser.start();
+        int ilow = Integer.parseInt(args[0]);
+        int ihigh = Integer.parseInt(args[1]);
+        Guesser guesser = new Guesser(ilow, ihigh);
+        guesser.start();
 		
 	}
 

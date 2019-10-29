d83374d22878ecdf50cd41ca07c05754b47598a1
willsteady <56724468+willsteady@users.noreply.github.com>
2019-10-29 21:25:03 +0100

    Update Guesser.java
    Completed the start() method, so that
     in the method body, the rules() method is called first, next the doGuesses() method.

--- Guesser.java
+++ Guesser.java
@@ -16,3 +16,19 @@ public Guesser(int low, int high) {
 			this.high = high;
 
 		}
+	}
+
+public void start() {
+		//Guesser guesser = new Guesser(0,1000);
+		rules();
+		doGuesses();
+		
+	}
+	public String toString(){
+		  return "low: " + low + " high: " + high;
+		}
+	private void rules(){
+	    System.out.println("Think of a number between " + low + " and "  + high);
+	    System.out.println("I'm going to ask a few questions in order " + "to guess the number.");
+	    System.out.println("Please answer T for true, and F for false.\n");
+	  }

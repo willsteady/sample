e0be6663b4be26305feb887738e159e48932d911
willsteady <56724468+willsteady@users.noreply.github.com>
2019-10-29 21:31:18 +0100

    Update Guesser.java
    Wrote doGuesses() method

--- Guesser.java
+++ Guesser.java
@@ -49,3 +49,32 @@ public void start() {
         } while (correctAnswer == false);
         return reply;
 	  }
+private void doGuesses(){
+	    int i=0; // number of guesses
+	    int middle=0;
+	    while(low<high){
+	      // Set next guess to the middle between
+	      // current low and current high
+	      middle=low + (high-low)/2;
+
+	      System.out.println("Is the number less than or equal to " + middle + "?");
+	      String reply = getReply();
+	      if("T".equals(reply)){
+	        // The number is less than or equal to middle
+	        // so we move down high to middle
+	        high = middle;
+	      }else{
+	        // The number is greater than middle,
+	        // so we move up low to middle + 1
+	        low = middle + 1;
+	      }
+	      i++; // One more guess!
+	    }
+	    // When low has met high, we don't enter the loop
+	    // and we have found the number
+	    answer(low, i);
+	  }
+	private void answer(int guess, int numberOfGuesses){
+	    System.out.println("You were thinking about " + guess + " (took me " + numberOfGuesses + " guesses)");
+	}
+}
